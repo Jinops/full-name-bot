f67b91cf587165834793c71ea892a0a11ef852a3
Jinops <khutt@naver.com>
2019-10-31 00:34:35 +0900

    parse private.json

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,17 @@
 import os
+import json
 
 def lambda_handler(event, context):
 	# TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    incoming_webhook_url = get_json()
+    print (incoming_webhook_url)
+
+def get_json():
+	with open("private.json") as json_file:
+		# Sample of private.json 
+		# {"url" : "https://www.google.co.kr"}
+		json_data = json.load(json_file)
+		return json_data["url"]
+
+#FOR TEST
+lambda_handler("TEST", "TEST")
