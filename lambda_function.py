931fcb5e8234b0f4a77c1d2d4738b76d1ba6a43d
Jinops <khutt@naver.com>
2019-10-31 01:29:14 +0900

    [Feature] send a message to slack
    [Feature] send a message to slack when post_message() function is called

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,19 @@
 import os
 import json
+import requests
 
 def lambda_handler(event, context):
 	# TODO implement
-    incoming_webhook_url = get_json()
-    print (incoming_webhook_url)
+
+	payloads = {
+		"attachments" : [{
+			"text" : "[JGQ] 1. Jungle Quests",
+			"color" : "#3d0040",
+		}]
+	}
+
+	incoming_webhook_url = get_json()
+	post_message(incoming_webhook_url, payloads)
 
 def get_json():
 	with open("private.json") as json_file:
@@ -12,6 +21,16 @@ def get_json():
 		# {"url" : "https://www.google.co.kr"}
 		json_data = json.load(json_file)
 		return json_data["url"]
+
+def post_message(url, payloads):
+
+	response = requests.post(url, json.dumps(payloads))
+	if response.status_code == 200:
+		print("success")
+	else :
+		print("%s error" %response.status_code)
+
+
 
 #FOR TEST
 lambda_handler("TEST", "TEST")
