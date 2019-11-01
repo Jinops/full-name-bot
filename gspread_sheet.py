fc452a4c8e4047003ddb891d1b85f744907c3fd8
Jinops <khutt@naver.com>
2019-11-01 23:15:12 +0900

    [Feature] Get data from google sheet

--- gspread_sheet.py
+++ gspread_sheet.py
@@ -1,6 +1,15 @@
 import gspread
+from private import permission_json_file, sheet_url, sheet_name
 
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('Full name Bot-0207e416d2b3.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(permission_json_file, scope)
+gc = gspread.authorize(credentials)
+
+doc = gc.open_by_url(sheet_url)
+worksheet = doc.worksheet(sheet_name)
+
+#colum B(2) : ID | colum D(4) : Game Name | colum E(5) : Game Title
+colum_data = worksheet.col_values(5)
+print(colum_data)
