19589cfce27f5030b57183eefb1e0b14359bb914
Jinops <khutt@naver.com>
2019-11-01 23:13:29 +0900

    [Modify] Update private.py and set gitignore

--- private_sample.py
+++ private_sample.py
@@ -1,2 +1,8 @@
+#your slack incoming webhook URL
 incoming_webhook_url = 	"https://hooks.slack.com/services/MY_URL"
+#json file name that you got from google apis
+permission_json_file = "file_name.json"
+#your google sheet URL ** permission required
 sheet_url = "https://docs.google.com/spreadsheets/d/MY_URL"
+#your google sheet name (not the file name)
+sheet_name = "sample_sheet"
