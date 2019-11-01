8a7639b6d745904a6e5543677ef835be942113db
Jinops <khutt@naver.com>
2019-11-01 23:17:22 +0900

    fixed untracked files

--- private.py
+++
@@ -1,2 +0,0 @@
-incoming_webhook_url = 	"https://hooks.slack.com/services/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"
-sheet_url = "https://docs.google.com/spreadsheets/d/1FYWnXQl3gPSLieqHqbfFwjjHL3cyMX6S9i6-ThAtUWU/edit?usp=sharing"
