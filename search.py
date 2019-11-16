0d8c875bdeeb655c9c997ecab500ea3e08c5c003
Jinops <jinwoo.develop@gmail.com>
2019-11-16 18:01:32 +0900

    [Modify] Change variable anme

--- search.py
+++ search.py
@@ -9,13 +9,11 @@ update_cycle = 30 # Days
 
 
 def search():
-	global sheet
-	#is_time_passed(datetime.now(), datetime.timedelta(days=30))
-	#if (datetime.today() )
 	load_json()
-	#print(is_time_passed(live_time, update_cycle))
-	#	save_sheet()
+	get_all_title()
 
+
+###########################
 def load_json():
 	global sheet
 	if not os.path.exists("sheet.json"):
@@ -29,18 +27,22 @@ def load_json():
 			save_sheet(live_time.strftime("%Y-%m-%d"))
 			print("sheet is updated")
 
-
-	
-
-
 def is_update_cylce(live_time, period):
 	global sheet
 	last_updated_time = datetime.strptime(sheet[0], "%Y-%m-%d")
 	day_passed = (live_time - last_updated_time).days
+	#return True ##TEST
 	if (day_passed >= 30):
 		return True
 	else :
 		return False
+###########################
 
+def get_all_title():
+	global sheet
+	all_title = []
+	for title in sheet :
+		all_title.append(title[4])
+	print(all_title)
 
 search()
