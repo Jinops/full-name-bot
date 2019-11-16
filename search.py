efbe3ec2dd5388c9c100d2e8eca90142a06b016a
Jinops <jinwoo.develop@gmail.com>
2019-11-16 17:17:15 +0900

    [Feature] auto-update sheet.json in cycle
    optimization

--- search.py
+++ search.py
@@ -5,32 +5,42 @@ from datetime import datetime
 
 live_time = datetime.today()
 sheet = []
+update_cycle = 30 # Days
 
 
 def search():
 	global sheet
 	#is_time_passed(datetime.now(), datetime.timedelta(days=30))
 	#if (datetime.today() )
-	sheet = load_json()
-	last_updated_time = sheet[0]
-	print(last_updated_time)
+	load_json()
+	#print(is_time_passed(live_time, update_cycle))
 	#	save_sheet()
 
-
-def update_checker():
-	sprint (live_time)
-
 def load_json():
+	global sheet
 	if not os.path.exists("sheet.json"):
 		save_sheet(live_time.strftime("%Y-%m-%d"))
-		
+		print("sheet is created")
+	
 	with open('sheet.json') as json_file:
-		sheet_json = json.load(json_file)
-	return sheet_json
+		sheet = json.load(json_file)
+
+	if is_update_cylce(live_time, update_cycle) :
+			save_sheet(live_time.strftime("%Y-%m-%d"))
+			print("sheet is updated")
 
 
-#def is_time_passed(live_time, period):
+	
 
+
+def is_update_cylce(live_time, period):
+	global sheet
+	last_updated_time = datetime.strptime(sheet[0], "%Y-%m-%d")
+	day_passed = (live_time - last_updated_time).days
+	if (day_passed >= 30):
+		return True
+	else :
+		return False
 
 
 search()
